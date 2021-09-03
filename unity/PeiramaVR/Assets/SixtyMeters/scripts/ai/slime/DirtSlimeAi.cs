424a0078b6a1da7dc23cb2e2d6487bf14f5b8bf3
Theo Winter <theo.winter@gmail.com>
2021-09-03 21:16:48 +0200

    Improved slime AI with rate limiting and random movements when not fleeing from a broom

--- unity/PeiramaVR/Assets/SixtyMeters/scripts/ai/slime/DirtSlimeAi.cs
+++ unity/PeiramaVR/Assets/SixtyMeters/scripts/ai/slime/DirtSlimeAi.cs
@@ -7,10 +7,14 @@ namespace SixtyMeters.scripts.ai.slime
     {
         // The game object from which the AI should flee from
         private GameObject _runAwayFrom;
-
-        public float fearDistance = 2;
-        
         private NavMeshAgent _navMeshAgent;
+        private const float RateLimit = 2;
+        private float _nextMovementCheck = 0;
+        private int _walkableNavMeshArea;
+
+        public float fearMovementDistance = 2;
+        public float randomMovementDistance = 3;
+
 
         // Start is called before the first frame update
         void Start()
@@ -25,48 +29,58 @@ namespace SixtyMeters.scripts.ai.slime
             }
 
             _navMeshAgent = GetComponent<NavMeshAgent>();
+            _walkableNavMeshArea = NavMesh.GetAreaFromName("Walkable");
         }
 
         // Update is called once per frame
         void Update()
         {
-            //TODO: rate limit check & runaway
-            float dist = Vector3.Distance(transform.position, _runAwayFrom.transform.position);
-            if(dist < fearDistance)
+            // Rate-limiting the next movement check to improve performance and to make the slime a bit easier to
+            // catch for the player
+            if (Time.time > _nextMovementCheck)
             {
-                //TODO: run away
-                RunFrom();
-            }
-            else
-            {
-                //TOOD: pick random destination
-            }
+                float dist = Vector3.Distance(transform.position, _runAwayFrom.transform.position);
+                if (dist < fearMovementDistance)
+                {
+                    RunAwayFromBroom();
+                }
+                else
+                {
+                    RandomMovement();
+                }
 
+                _nextMovementCheck = Time.time + RateLimit;
+            }
         }
-        
-        public void RunFrom()
+
+        private void RunAwayFromBroom()
         {
- 
-            // store the starting transform
-            var startTransform = transform;
-         
             //temporarily point the object to look away from the player
             var groundedRunAwayFromPos = _runAwayFrom.transform.position;
             groundedRunAwayFromPos.y = transform.position.y;
             transform.rotation = Quaternion.LookRotation(transform.position - groundedRunAwayFromPos);
- 
+
             //Then we'll get the position on that rotation that's multiplyBy down the path (you could set a Random.range
             // for this if you want variable results) and store it in a new Vector3 called runTo
-            Vector3 runTo = transform.position + transform.forward * 2;
+            var runTo = transform.position + transform.forward * 2;
 
             //So now we've got a Vector3 to run to and we can transfer that to a location on the NavMesh with samplePosition.
             // 5 is the distance to check
-            NavMesh.SamplePosition(runTo, out var hit, 5, 1 << NavMesh.GetNavMeshLayerFromName("Walkable"));
+            NavMesh.SamplePosition(runTo, out var hit, 5, 1 << _walkableNavMeshArea);
 
-            // reset the transform back to our start transform
-            transform.position = startTransform.position;
-            transform.rotation = startTransform.rotation;
- 
+            // And get it to head towards the found NavMesh position
+            _navMeshAgent.SetDestination(hit.position);
+        }
+        
+        private void RandomMovement()
+        {
+            var randomDirection = Random.insideUnitSphere * randomMovementDistance;
+            randomDirection += transform.position;
+
+            //So now we've got a Vector3 to run to and we can transfer that to a location on the NavMesh with samplePosition.
+            // 5 is the distance to check
+            NavMesh.SamplePosition(randomDirection, out var hit, randomMovementDistance, 1 << _walkableNavMeshArea);
+            
             // And get it to head towards the found NavMesh position
             _navMeshAgent.SetDestination(hit.position);
         }
