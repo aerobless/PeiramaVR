58569e90ac7b90e834ce4706763b8ee0e1d59c9d
Theo Winter <theo.winter@gmail.com>
2021-11-12 12:32:31 +0100

    Updated .gitignore for puppetmaster, added goblin ai base

---
+++ unity/SkyInn/Assets/SixtyMeters/characters/monsters/goblins/ai/GoblinAI.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace SixtyMeters.characters.monsters.goblins.ai
+{
+    public class GoblinAI : MonoBehaviour
+    {
+        
+        //TODO: destroy when falling into void
+        //TODO: spawn ragdoll on death
+        //TODO: basic intent to go to inn
+        //TODO: fight when in range of player
+        //TODO: follow player
+        //TODO: squad ai, goblins of same squad will stay together
+        //TODO: can maybe be tamed when offering food instead of fight
+        //TODO: disable navmesh when agent is pushed so they can be pushed off the islands
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
