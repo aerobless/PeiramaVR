91d72d459d05874ef31ec7f5dd53cd96f37a5bd8
Theo Winter <theo.winter@gmail.com>
2021-11-14 14:10:01 +0100

    Added basic animation blending for goblin and expanded script to use animations

--- unity/SkyInn/Assets/SixtyMeters/characters/monsters/goblins/ai/GoblinAI.cs
+++ unity/SkyInn/Assets/SixtyMeters/characters/monsters/goblins/ai/GoblinAI.cs
@@ -1,3 +1,4 @@
+using System;
 using SixtyMeters.scripts.helpers.waypoints;
 using SixtyMeters.scripts.level;
 using UnityEngine;
@@ -17,6 +18,7 @@ namespace SixtyMeters.characters.monsters.goblins.ai
         //TODO: disable navmesh when agent is pushed so they can be pushed off the islands
         
         private NavMeshAgent _navMeshAgent;
+        private Animator _animator;
         private InnLevelManager _innLevelManager;
         private GameObject _player;
 
@@ -27,10 +29,15 @@ namespace SixtyMeters.characters.monsters.goblins.ai
         private const float RateLimit = 1;
         private const float PlayerAggressionRange = 5;
         
+        Vector2 smoothDeltaPosition = Vector2.zero;
+        Vector2 velocity = Vector2.zero;
+        
         // Start is called before the first frame update
         void Start()
         {
             _navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+            _navMeshAgent.updatePosition = false;
+            _animator = gameObject.GetComponentInChildren<Animator>();
             _player = GameObject.Find("PlayerController");
             SetupInnLevelManager();
             GoToKitchen();
@@ -39,6 +46,27 @@ namespace SixtyMeters.characters.monsters.goblins.ai
         // Update is called once per frame
         void Update()
         {
+            Vector3 worldDeltaPosition = _navMeshAgent.nextPosition - transform.position;
+
+            // Map 'worldDeltaPosition' to local space
+            float dx = Vector3.Dot (transform.right, worldDeltaPosition);
+            float dy = Vector3.Dot (transform.forward, worldDeltaPosition);
+            Vector2 deltaPosition = new Vector2 (dx, dy);
+
+            // Low-pass filter the deltaMove
+            float smooth = Mathf.Min(1.0f, Time.deltaTime/0.15f);
+            smoothDeltaPosition = Vector2.Lerp (smoothDeltaPosition, deltaPosition, smooth);
+
+            // Update velocity if time advances
+            if (Time.deltaTime > 1e-5f)
+                velocity = smoothDeltaPosition / Time.deltaTime;
+
+            //bool shouldMove = velocity.magnitude > 0.5f && _navMeshAgent.remainingDistance > _navMeshAgent.radius;
+
+            //GetComponent<LookAt>().lookAtTargetPosition = _navMeshAgent.steeringTarget + transform.forward;
+            _animator.SetFloat("VelocityX", velocity.x/10);
+            _animator.SetFloat("VelocityY", velocity.y/10);
+            
             if (Time.time > _nextMovementCheck)
             {
                 // It may be more realistic to do a ray cast or similar in a cone from the goblins face so that it
@@ -53,6 +81,17 @@ namespace SixtyMeters.characters.monsters.goblins.ai
                 }
                 _nextMovementCheck = Time.time + RateLimit;
             }
+            if (worldDeltaPosition.magnitude > _navMeshAgent.radius)
+                _navMeshAgent.nextPosition = transform.position + 0.9f*worldDeltaPosition;
+        }
+        
+        public void AnimatorMove ()
+        {
+            // Update position to agent position
+            Vector3 position = _animator.rootPosition;
+            position.y = _navMeshAgent.nextPosition.y;
+            transform.position = position;
+            
         }
 
         private float GetDistanceToPlayer()
---
+++ unity/SkyInn/Assets/SixtyMeters/characters/monsters/goblins/ai/GoblinAnimator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SixtyMeters.characters.monsters.goblins.ai
+{
+    public class GoblinAnimator  : MonoBehaviour
+    {
+        private GoblinAI _goblinAI;
+        
+        void Start()
+        {
+            _goblinAI = gameObject.GetComponentInParent<GoblinAI>();
+        }
+        void OnAnimatorMove ()
+        {
+            _goblinAI.AnimatorMove();
+        }
+    }
+}
