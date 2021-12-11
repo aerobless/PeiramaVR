37311008e166c3b779f72a6a7048a9efd45dcba6
Theo Winter <theo.winter@gmail.com>
2021-12-11 22:29:02 +0100

    Added support to change height settings

--- unity/SkyInn/Assets/SixtyMeters/models/ui/pad/scripts/SettingsPanel.cs
+++ unity/SkyInn/Assets/SixtyMeters/models/ui/pad/scripts/SettingsPanel.cs
@@ -1,3 +1,4 @@
+using System;
 using HurricaneVR.Framework.Core.Player;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,27 +13,30 @@ namespace SixtyMeters.models.ui.pad.scripts
         public Slider snapTurningRateSlider;
 
         public ToggleBehaviour smoothTurningToggle;
-    
+
+        public Toggle sitting;
+        public Toggle standing;
+        public Toggle height;
+
         // Start is called before the first frame update
         void Start()
         {
             // Initialize UI controls
-            snapTurningRateSlider.value = playerController.SnapAmount;
+            snapTurningRateSlider.value = playerController.SnapAmount / 100;
             smoothTurningToggle.setValue(playerController.RotationType == RotationType.Smooth);
         }
 
         // Update is called once per frame
         void Update()
         {
-            
         }
-        
+
         public void CalibrateHeight()
         {
             if (cameraRig)
                 cameraRig.Calibrate();
         }
-        
+
         public void UpdateSmoothTurning()
         {
             playerController.RotationType = smoothTurningToggle.getValue() ? RotationType.Smooth : RotationType.Snap;
@@ -40,8 +44,41 @@ namespace SixtyMeters.models.ui.pad.scripts
 
         public void UpdateSnapTurnRate()
         {
-            playerController.SnapAmount = snapTurningRateSlider.value*100;
+            playerController.SnapAmount = snapTurningRateSlider.value * 100;
         }
 
+        public void UpdateSittingStandingHeight(String type)
+        {
+            switch (type)
+            {
+                case "Sitting":
+                    if (sitting.isOn)
+                    {
+                        cameraRig.SetSitStandMode(HVRSitStand.Sitting);
+                        standing.isOn = false;
+                        height.isOn = false;
+                    }
+
+                    break;
+                case "Standing":
+                    if (standing.isOn)
+                    {
+                        cameraRig.SetSitStandMode(HVRSitStand.Standing);
+                        sitting.isOn = false;
+                        height.isOn = false;
+                    }
+
+                    break;
+                case "PlayerHeight":
+                    if (height.isOn)
+                    {
+                        cameraRig.SetSitStandMode(HVRSitStand.PlayerHeight);
+                        sitting.isOn = false;
+                        standing.isOn = false;
+                    }
+
+                    break;
+            }
+        }
     }
 }
