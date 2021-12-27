d313c4243f9ce26436a6a94178b633dde15e45de
Theo Winter <theo.winter@gmail.com>
2021-12-27 20:23:09 +0100

    Added shader for cookable food items. It can blend between 3 textures (uncooked, cooked, burned) Added logic that lerps between these blend values.

---
+++ unity/SkyInn/Assets/SixtyMeters/models/kitchen/food/scripts/Cookable.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+namespace SixtyMeters.models.kitchen.food.scripts
+{
+    public class Cookable : MonoBehaviour
+    {
+        private Renderer _renderer;
+
+        public float cookTimeSec = 30;
+        public bool canBurn = true;
+
+        public bool _isCooking = false;
+        
+        private float _timeCooked = 0;
+        private static readonly int CookRate = Shader.PropertyToID("_cookRate");
+        private static readonly int BurnRate = Shader.PropertyToID("_burnRate");
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _renderer = GetComponent<Renderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_isCooking)
+            {
+                var cookValue = Mathf.Lerp(0, 1, _timeCooked / cookTimeSec);
+                if (cookValue > 1)
+                {
+                    cookValue = 1;
+                }
+                _renderer.material.SetFloat(CookRate, cookValue);
+
+                if (_timeCooked > cookTimeSec)
+                {
+                    var burnValue = Mathf.Lerp(0, 1, _timeCooked / (cookTimeSec*2));
+                    if (burnValue > 1)
+                    {
+                        burnValue = 1;
+                    }
+                    _renderer.material.SetFloat(BurnRate, burnValue);
+                }
+                
+                _timeCooked += Time.deltaTime;
+            }
+        }
+
+        public void StartCooking()
+        {
+            _isCooking = true;
+        }
+        
+        public void StopCooking()
+        {
+            _isCooking = false;
+        }
+    }
+}
---
+++ unity/SkyInn/Assets/SixtyMeters/models/kitchen/food/scripts/StoveBehaviour.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+namespace SixtyMeters.models.kitchen.food.scripts
+{
+    public class StoveBehaviour : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var food = other.GetComponent<Cookable>();
+            if (food)
+            {
+                food.StartCooking();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            var food = other.GetComponent<Cookable>();
+            if (food)
+            {
+                food.StopCooking();
+            }
+        }
+    }
+}
